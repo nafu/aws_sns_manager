a88201b435ba63743cd3cf096dda748cd3708e2c
Fumiya Nakamura <nakamurafumiya003@gmail.com>
2015-04-15 22:32:17 +0900

    Add AwsSnsManager.config

--- lib/aws_sns_manager.rb
+++ lib/aws_sns_manager.rb
@@ -3,5 +3,10 @@ require 'json'
 
 # = AwsSnsManager
 module AwsSnsManager
+  autoload :Config, 'aws_sns_manager/config'
   autoload :Client, 'aws_sns_manager/client'
+
+  def self.config(&block)
+    block.call(AwsSnsManager::Config) if block_given?
+  end
 end
---
+++ spec/aws_sns_manager_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe AwsSnsManager do
+  describe '.config' do
+    it 'only uses the model' do
+      expect(AwsSnsManager::Config.model).to eq('PushEndpoint')
+      AwsSnsManager.config do |config|
+        config.model = 'UserDefine'
+      end
+      expect(AwsSnsManager::Config.model).to eq('UserDefine')
+    end
+  end
+end
